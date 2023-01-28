876bbe57703a60c65ff78884fbdf01d8608616f6
Hayate_T <hayatesato1@gmail.com>
2023-01-28 21:06:47 +0900

    編集、削除完成

--- app/controllers/movie_posts_controller.rb
+++ app/controllers/movie_posts_controller.rb
@@ -1,6 +1,6 @@
 class MoviePostsController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.all.order(updated_at: "DESC")
   end
 
   def new
@@ -14,6 +14,36 @@ class MoviePostsController < ApplicationController
       redirect_to "/movie", notice: "投稿に成功"
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  # 詳細ページ
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  # 更新する
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update(movies_params)
+      redirect_to "/movie/show/#{@movie.id}", notice: "更新に成功"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @movie = Movie.find(params[:id])
+
+    if @movie.destroy
+      redirect_to "/movie", notice: "削除に成功"
+    else
+      redirect_to "/movie", error: "削除に失敗"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   # ! movie用のru0ティング
   get "/movie", to: "movie_posts#index"
   get "/movie/new", to: "movie_posts#new"
-  post "/movie/new", to: "movie_posts#create"
+  post "/movie/create", to: "movie_posts#create"
+  get "/movie/show/:id", to: "movie_posts#show"
+  get "/movie/edit/:id", to: "movie_posts#edit"
+  patch "/movie/update/:id", to: "movie_posts#update"
+  delete "/movie/delete/:id", to: "movie_posts#delete"
 end
